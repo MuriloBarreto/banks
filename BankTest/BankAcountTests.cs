3242df0255df8d2571013dd4528f81060e784c1f
MURILO BARRETO ALMEIDA <murilo.balmeida1@senacsp.edu.br>
2020-11-18 11:34:02 -0300

    Adicionar arquivos de projeto.

---
+++ BankTest/BankAcountTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAcountTests
+    {
+        [TestMethod]
+        public void Debit_withValidAmount_UpdateBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("muri", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, account, "Débito não ocorreu corretamente.");
+        }
+    }
+}
