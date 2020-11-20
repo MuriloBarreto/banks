bc9451715795a399df703e38108b57010eb0bd59
Murilo Barreto <barreto_11almeida@hotmail.com>
2020-11-19 22:16:07 -0300

    Update

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -4,6 +4,8 @@ namespace Bank
 {
     public class BankAccount
     {
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
         //atribustos
         private readonly string m_customerName;
         private double m_balance;
@@ -31,11 +33,11 @@ namespace Bank
         {
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
             m_balance -= amount;
         }
